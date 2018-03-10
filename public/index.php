d3517e4e3eb596b1a9c13214b46f27762be6f349
Maksim Vetryakov <mvetryakov@gmail.com>
2018-03-10 22:54:15 +0300

    fix

--- public/index.php
+++ public/index.php
@@ -183,3 +183,37 @@ file_put_contents('request.log', $logData, ($filetime < $yesterday) ?: FILE_APPE
 #$REPOSITORY_NAME   $2
 #$REPOSITORY_BRANCH $3
 
+
+
+/**
+ * Gets the value of an environment variable. Supports boolean, empty and null.
+ *
+ * @param  string  $key
+ * @param  mixed   $default
+ * @return mixed
+ */
+function env($key, $default = null)
+{
+    $value = getenv($key);
+    if ($value === false) {
+        return value($default);
+    }
+    switch (strtolower($value)) {
+        case 'true':
+        case '(true)':
+            return true;
+        case 'false':
+        case '(false)':
+            return false;
+        case 'empty':
+        case '(empty)':
+            return '';
+        case 'null':
+        case '(null)':
+            return;
+    }
+    if (Str::startsWith($value, '"') && Str::endsWith($value, '"')) {
+        return substr($value, 1, -1);
+    }
+    return $value;
+}
